cf4a602802842a5414b00427bb163ec7c11eedd5
C0libry <pe.pe.l@yandex.ru>
2023-10-17 12:15:26 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,11 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        
+        'public_uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/uploads',
+        ],
 
         's3' => [
             'driver' => 's3',
